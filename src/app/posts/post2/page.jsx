2a02092cc6d69d9375805501dc43184f2217eb25
PedroHenriqueLOURENCO <rm98402@fiap.com.br>
2023-10-06 20:59:42 -0300

    Posts criados e imagens adicionadas em cada.

---
+++ src/app/posts/post2/page.jsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Post1() {
+  return (
+    <div>
+        <h1>POST - 1</h1>
+        <div>
+            <figure>
+             <Image src="/img/hamburguer.png" alt="junk food" width={350} height={250}/>
+             <figcaption>
+                <Link  href="/">Voltar</Link>
+            </figcaption>  
+            </figure>
+        </div>
+    </div>
+  )
+}
